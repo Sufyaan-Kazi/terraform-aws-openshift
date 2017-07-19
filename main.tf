a66720181b0f8b99e61d86d9d447214294d0bc56
Sufyaan Kazi <sufyaan_kazi@hotmail.com>
2017-07-19 16:34:19 +0100

    Initial Changes

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 module "openshift" {
   source          = "./modules/openshift"
   region          = "${var.region}"
-  amisize         = "t2.large"    //  Smallest that meets the min specs for OS
+  amisize         = "t2.medium"    //  Smallest that meets the min specs for OS
   vpc_cidr        = "10.0.0.0/16"
   subnetaz        = "${var.subnetaz}"
   subnet_cidr     = "10.0.1.0/24"
